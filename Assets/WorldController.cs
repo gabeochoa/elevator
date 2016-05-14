3ad2d286d09322ec57890f871b3e075ff6da5b34
Gabe <gabe1118@yahoo.com>
2016-05-14 18:46:07 -0400

    UnityProject

---
+++ Assets/WorldController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour {
+
+	public World world;
+	public int width;
+	public int height;
+	// Use this for initialization
+	void Start () {
+		world = new World (width, height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		world.update ();
+	}
+}
