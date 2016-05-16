329a4bb674072c2ece4389c627c7809b1d672e2a
Gabriel Ochoa <gabe1118@yahoo.com>
2016-05-15 21:54:06 -0400

    Update main.cs

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -11,17 +11,17 @@ public class main
 {                                                                                                                                            
     static void Main(string[] args)                                                                                                     
     {     
-		/*
-        World w = new World(40, 40);
-
-        int i = 0;
-        while(i < 10)
-        {
-            w.update();
-            i++;
-
-            System.Threading.Thread.Sleep(1000);
-        }                
-		*/
+	/*
+	        World w = new World(40, 40);
+	
+	        int i = 0;
+	        while(i < 10)
+	        {
+	            w.update();
+	            i++;
+	
+	            System.Threading.Thread.Sleep(1000);
+	        }                
+	*/
     }                                                                                                                                          
 }  
